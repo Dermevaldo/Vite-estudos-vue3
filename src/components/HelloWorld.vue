e067c4249e606d75dfa2af2b4e59c7ba541a6d25
Dermevaldo <dermeva@gmail.com>
2022-06-21 11:04:11 -0300

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,23 +4,42 @@ export default {
     return {
       color: 'red',
       count: 10,
+      notes: [],
+      noteTemp: {},
     };
+  },
+  methods: {
+    save() {
+      this.notes.push({
+        text: this.noteTemp.text,
+        creator: this.noteTemp.creator,
+      });
+      this.noteTemp = {
+        text: '',
+        creator: '',
+      };
+    },
   },
 };
 </script>
 
 <template>
-  <div style="" @click="count += 10">
-    {{ count }}
+  <input type="text" v-model="noteTemp.text" />
+  <br />
+  <input type="text" v-model="noteTemp.creator" />
+
+  <div class="div" @click="save">Salvar</div>
+  <div>
+    <p v-if="notes" v-for="note in notes">
+      Texto:{{ note.text }} Criador:{{ note.creator }}
+    </p>
   </div>
 </template>
 
 <style scoped>
-div {
+.div {
   background-color: blue;
-  width: 50px;
-  height: v-bind('count');
+  height: 50px;
   color: white;
-  top: ;
 }
 </style>
