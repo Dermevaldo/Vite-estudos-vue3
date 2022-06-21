9ce714952a085a34f190662c5e269fd58aee2eee
Dermevaldo <dermeva@gmail.com>
2022-06-21 10:21:48 -0300

    2 files modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,26 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+<script LANG="ts">
+export default {
+  data() {
+    return {
+      color: 'red',
+      count: 10,
+    };
+  },
+};
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div style="" @click="count += 10">
+    {{ count }}
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+div {
+  background-color: blue;
+  width: 50px;
+  height: v-bind('count');
+  color: white;
+  top: ;
 }
 </style>
